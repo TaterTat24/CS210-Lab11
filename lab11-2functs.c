8018cdad5ace620ccc747883fb044921d57ffb5a
Benjamin <C24Benjamin.Tat@afacademy.af.edu>
2022-09-13 21:16:31 -0600

    Lab 11 Solved

--- lab11-2functs.c
+++ lab11-2functs.c
@@ -6,20 +6,46 @@
 * Purpose: Implementation file for lab11-2.h
 * ===========================================================
 */
-#include <stdbool.h>
+#include <stdbool.h>// do I even need this? why not all libraries in the h file
 #include <ctype.h>
+
+#include "lab11-2functs.h"
 
 bool isFirstHalfAlpha(char theChar) {
     theChar = toupper(theChar);
+    if ((theChar >= 65) && (theChar <= 77)) {
+        return true;
+    }
     return false;
 }
 
-int typeOfChar() {
-
-    return 0;
+int typeOfChar(char theChar) {
+    if (isalpha(theChar)) {
+        theChar = toupper(theChar);
+        switch (theChar) {
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+            case 'Y':
+                return CHAR_IS_VOWEL;
+                break;
+            default:
+                return CHAR_IS_CONSONANT;
+                break;
+        }
+    }
+    else {
+        return CHAR_NOT_ALPHA;
+    }
 }
 
-bool isMagicChar() {
-    // For now, this function always returns false.  We will fix it later!
-    return false;
+bool isMagicChar(char theChar) {
+    if (theChar == MAGIC_CHAR) {
+        return true;
+    }
+    else{
+        return false;
+    }
 }
